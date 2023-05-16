7fcd1944f5f1feae5e92dae9c324c549256c8dd1
Facu-J-Boy <boyfacundo45@gmail.com>
2023-05-16 20:34:36 -0300

    createUser controller

---
+++ api/src/routes/controllers/User.ts
@@ -0,0 +1,14 @@
+import { User } from "models/User";
+import { NullishPropertiesOf } from "sequelize/types/utils";
+
+export const controller = {
+    createUser: async (datos: Omit<User, NullishPropertiesOf<User>>) => {
+        try {
+            const newUser = await User.create (datos);
+            return newUser;            
+        } catch (error) {
+            console.error('Error al crear el usuario', error);
+            throw error;
+        }
+    }
+}

756396546975b1aa07b59a48054019d6c7949bec
Facu-J-Boy <boyfacundo45@gmail.com>
2023-05-22 18:39:10 -0300

    User component

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import ToDoList from './ToDoList/ToDoList';
 import LoginScreen from './LoginScreen/LoginScreen';
+import User from './User/User';
 
 const App: React.FC = ():JSX.Element => {
   return (
     <>
     <Routes>
       <Route path='/' element={<LoginScreen />} />
-      <Route path='/user' element={<ToDoList />} />
+      <Route path='/user' element={<User />} />
      </Routes>
     </>
   )
---
+++ client/src/components/User/User.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ToDoList from '../ToDoList/ToDoList';
+
+const User = () => {
+  return (
+    <div>
+        <ToDoList />
+    </div>
+  )
+}
+
+export default User

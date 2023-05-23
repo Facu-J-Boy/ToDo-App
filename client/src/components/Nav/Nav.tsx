cfff4e26ca09bb487fc306d25c8d38a92271e8c6
Facu-J-Boy <boyfacundo45@gmail.com>
2023-05-22 21:54:24 -0300

    Nav component

--- client/src/components/Nav/Nav.tsx
+++ client/src/components/Nav/Nav.tsx
@@ -3,8 +3,11 @@ import LogoSmall from '../LogoSmall/LogoSmall';
 
 const Nav = () => {
   return (
-    <div>
-        <LogoSmall />
+    <div className='flex flex-row relative p-5 border-b-2 border-grey'>
+        <div className='ml-10'>
+          <LogoSmall />
+        </div>
+          <button className='right-0 absolute mr-5 bg-green p-2 mb-2 rounded-sm text-white'> Log out </button>
     </div>
   )
 }

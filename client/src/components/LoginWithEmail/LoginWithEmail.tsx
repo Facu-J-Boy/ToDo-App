5ca0d0cdad389bf98fa816ead0846e344f70f9af
Facu-J-Boy <boyfacundo45@gmail.com>
2023-05-23 21:25:50 -0300

    little change

--- client/src/components/LoginWithEmail/LoginWithEmail.tsx
+++ client/src/components/LoginWithEmail/LoginWithEmail.tsx
@@ -94,7 +94,7 @@ const LoginWithEmail: React.FC = (): JSX.Element => {
                           "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"}
                         value={password}
                         onInput={handlePasswordChange} />
-                        {passwordError && <p className="text-red font-medium">Minimo 6 caracteres.</p>}
+                        {passwordError && <p className="text-red font-medium">Min 6 characters.</p>}
                     </div>
                     <button type="submit" 
                     className="w-full text-white bg-lightGreen focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"

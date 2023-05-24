03517823223a35e66db139900fff332cb94b1852
Facu-J-Boy <boyfacundo45@gmail.com>
2023-05-24 11:47:20 -0300

    login with email input password visibility

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center items-center h-screen'>
         <p className={`inline text-9xl text-end font-bold`}>ToD</p>
         <img className="w-32 h-32" src='https://res.cloudinary.com/dvqh0exq6/image/upload/v1684796220/ToDo/approval-symbol-check-mark-in-a-circle-drawn-by-hand-vector-green-sign-ok-approval-or_y8zl8g.jpg' />
     </div>

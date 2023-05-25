acc723f7bcd553379d19a7b8cd73654a955b3d41
Facu-J-Boy <boyfacundo45@gmail.com>
2023-05-24 21:07:46 -0300

    logo link change

--- client/src/components/LogoSmall.tsx
+++ client/src/components/LogoSmall.tsx
@@ -4,7 +4,7 @@ const LogoSmall = () => {
   return (
     <div className='flex justify-center'>
         <p className="inline text-3xl text-end font-bold">ToD</p>
-        <img className="w-9 h-9" src='https://res.cloudinary.com/dvqh0exq6/image/upload/v1684796220/ToDo/approval-symbol-check-mark-in-a-circle-drawn-by-hand-vector-green-sign-ok-approval-or_y8zl8g.jpg' />
+        <img className="w-9 h-9" src='https://res.cloudinary.com/dvqh0exq6/image/upload/v1684972785/ToDo/grid_landscape-removebg-preview_m5yhof.png' />
     </div>
   )
 }

77f08b670544e27ed9af47953d5370e9eae4d7c5
Facu-J-Boy <boyfacundo45@gmail.com>
2023-05-25 19:00:26 -0300

    getUser action

--- client/src/Redux/Actions/index.ts
+++ client/src/Redux/Actions/index.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { Dispatch } from "redux";
 
 export const POST_USER = 'POST_USER';
+export const GET_USER = 'GET_USER';
 
 const url = "http://localhost:3001/api";
 
@@ -10,16 +11,30 @@ interface User {
     email: string
 }
 
+export const getUser = (id: string) => {
+    return async (dispatch: Dispatch) => {
+        try {
+            const user = await axios.get <User | null>(`${url}/user/${id}`);
+            return dispatch ({
+                type: GET_USER,
+                payload: user.data,
+            })
+        } catch (error) {
+            
+        }
+    }
+};
+
 export const postUser = (date: any) => {
     return async (dispatch: Dispatch) => {
         try {
-            const user = await axios.post <User>(`${url}/user`, date);
+            const newUser = await axios.post <User>(`${url}/user`, date);
             return dispatch ({
                 type: POST_USER,
-                payload: user.data
+                payload: newUser.data
             })
         } catch (error: any) {
             console.log(error.message)
         }
     }
-}
+};

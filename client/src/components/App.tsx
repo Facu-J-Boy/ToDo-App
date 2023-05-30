8d8f41e6cef55bff4f82616aedb21487ca24dd96
Facu-J-Boy <boyfacundo45@gmail.com>
2023-05-30 17:17:04 -0300

    login route

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,12 +3,14 @@ import {Routes, Route} from 'react-router-dom';
 import LoginScreen from './LoginScreen';
 import User from './User';
 import SignUpWithEmail from './SignUpWithEmail';
+import Logo from './Logo';
 
 const App: React.FC = ():JSX.Element => {
   return (
     <>
     <Routes>
-      <Route path='/' element={<LoginScreen/>} />
+      <Route path='/' element={<Logo/>} />
+      <Route path='/login' element ={<LoginScreen />} />
       <Route path='/signup' element={<SignUpWithEmail />} />
       <Route path='/user' element={<User />} />
      </Routes>

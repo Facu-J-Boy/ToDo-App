6c06c3b60be5f24b60851ca6e5208f6ad55fc908
Facu-J-Boy <boyfacundo45@gmail.com>
2023-06-01 18:20:37 -0300

    nav border change

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -5,7 +5,7 @@ import LogOut from './LogOut';
 const Nav: React.FC = (): JSX.Element => {
 
   return (
-    <div className='flex flex-row relative p-5 border-b-2 border-grey'>
+    <div className='flex flex-row relative p-5 border-b border-grey'>
         <div className='ml-10'>
           <LogoSmall />
         </div>

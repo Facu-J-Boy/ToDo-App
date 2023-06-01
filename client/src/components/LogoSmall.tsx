fcbc306224f44632a46b0b798397b052450f3132
Facu-J-Boy <boyfacundo45@gmail.com>
2023-06-01 17:06:38 -0300

    image in logo small

--- client/src/components/LogoSmall.tsx
+++ client/src/components/LogoSmall.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 const LogoSmall = () => {
   return (
     <div className='flex justify-center'>
-        <p className="inline text-3xl text-end font-bold">ToD</p>
-        <img className="w-9 h-9" src='https://res.cloudinary.com/dvqh0exq6/image/upload/v1684972785/ToDo/grid_landscape-removebg-preview_m5yhof.png' />
+        <img className="w-24" src='https://res.cloudinary.com/dvqh0exq6/image/upload/v1685649063/ToDo/Captura_de_pantalla_2023-06-01_164509-removebg-preview_jikdtx.png' />
     </div>
   )
 }

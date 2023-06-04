7b0758ea66fb7c3a4e76631ec37fb515a765ef3e
Facu-J-Boy <boyfacundo45@gmail.com>
2023-06-04 19:54:20 -0300

    turn to alter

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,8 @@
 import {sequelize} from './src/db';
 import app from './src/app';
-import cors from 'cors';
-
-// app.use(
-//     cors({
-//      origin: 'http://localhost:3000',
-//      credentials: true,
-//      methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
-//      allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
-//     })
-//    );
 
 sequelize
- .sync({force: true, logging: false})
+ .sync({alter: true, logging: false})
  .then(() => {
   console.log('base de datos conectada! :D');
   app.listen(3001, function () {

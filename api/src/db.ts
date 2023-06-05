ab347071220885e5c756fe1a7a5b7d5dc2c319a8
Facu-J-Boy <boyfacundo45@gmail.com>
2023-06-05 20:14:37 -0300

    new dot env

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,6 +2,8 @@ import {Sequelize} from 'sequelize-typescript';
 import config from './lib/config';
 config;
 
+const {URL_DATABASE} = process.env;
+
 // export const sequelize = new Sequelize({
 //  dialect: 'postgres',
 //  database: config.dbName,
@@ -11,4 +13,4 @@ config;
 //  models: [__dirname + '/models'],
 // });
 
-export const sequelize = new Sequelize ("postgres://default:NfhR5uSsDUd6@ep-quiet-glitter-902094.us-east-1.postgres.vercel-storage.com:5432/verceldb")
+export const sequelize = new Sequelize (`${URL_DATABASE}`)

ebb46cad16eb077ae9783d9397d113635a9f0bac
Facu-J-Boy <boyfacundo45@gmail.com>
2023-06-05 20:09:39 -0300

    new

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,11 +2,13 @@ import {Sequelize} from 'sequelize-typescript';
 import config from './lib/config';
 config;
 
-export const sequelize = new Sequelize({
- dialect: 'postgres',
- database: config.dbName,
- password: config.dbPassword,
- username: config.dbUser,
- storage: ':memory:',
- models: [__dirname + '/models'],
-});
+// export const sequelize = new Sequelize({
+//  dialect: 'postgres',
+//  database: config.dbName,
+//  password: config.dbPassword,
+//  username: config.dbUser,
+//  storage: ':memory:',
+//  models: [__dirname + '/models'],
+// });
+
+export const sequelize = new Sequelize ("postgres://default:NfhR5uSsDUd6@ep-quiet-glitter-902094.us-east-1.postgres.vercel-storage.com:5432/verceldb")

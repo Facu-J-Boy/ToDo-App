82f6a3562c626d05e4ce518cd70fa4d546895c4f
Facu-J-Boy <boyfacundo45@gmail.com>
2023-06-05 21:36:04 -0300

    local

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,15 +2,15 @@ import {Sequelize} from 'sequelize-typescript';
 import config from './lib/config';
 config;
 
-const {URL_DATABASE} = process.env;
+// const {URL_DATABASE} = process.env;
 
-// export const sequelize = new Sequelize({
-//  dialect: 'postgres',
-//  database: config.dbName,
-//  password: config.dbPassword,
-//  username: config.dbUser,
-//  storage: ':memory:',
-//  models: [__dirname + '/models'],
-// });
+export const sequelize = new Sequelize({
+ dialect: 'postgres',
+ database: config.dbName,
+ password: config.dbPassword,
+ username: config.dbUser,
+ storage: ':memory:',
+ models: [__dirname + '/models'],
+});
 
-export const sequelize = new Sequelize (`${URL_DATABASE}`)
+// export const sequelize = new Sequelize (`${URL_DATABASE}`)

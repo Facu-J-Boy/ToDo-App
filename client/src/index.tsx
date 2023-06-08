779ff6b6ed257645e76f69866f3a4f1a39546f81
Facu-J-Boy <boyfacundo45@gmail.com>
2023-06-08 20:35:55 -0300

    reinstall package

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import store from './Redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <Provider store = {store}>
+    <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
